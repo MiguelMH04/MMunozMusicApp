cfe7a421600bb166f4daeff55a9d624666457d79
MiguelMH04 <mmh79381@lasallebajio.edu.mx>
2025-10-09 23:15:12 -0600

    Creación del modelo y servicio

---
+++ app/src/main/java/com/example/mmunozmusicapp/Models/Album.kt
@@ -0,0 +1,9 @@
+package com.example.mmunozmusicapp.Models
+
+data class Album(
+    val artist: String,
+    val description: String,
+    val id: String,
+    val image: String,
+    val title: String
+)
---
+++ app/src/main/java/com/example/mmunozmusicapp/Services/AlbumService.kt
@@ -0,0 +1,14 @@
+package com.example.mmunozmusicapp.Services
+
+import com.example.mmunozmusicapp.Models.Album
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface AlbumService {
+
+        @GET("albums")
+        suspend fun getAllAlbums() : List<Album>
+
+        @GET("albums/{id}")
+        suspend fun getAlbumById(@Path("id") id: Int) : Album
+    }
