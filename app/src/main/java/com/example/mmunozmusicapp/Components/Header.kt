a7952e3e1ae5b5237c81056c1e32478c4a8a50cc
MiguelMH04 <mmh79381@lasallebajio.edu.mx>
2025-10-11 22:41:32 -0600

    Creacion del Header

---
+++ app/src/main/java/com/example/mmunozmusicapp/Components/Header.kt
@@ -0,0 +1,95 @@
+package com.example.mmunozmusicapp.Components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mmunozmusicapp.ui.theme.LightPurple
+import com.example.mmunozmusicapp.ui.theme.MMunozMusicAppTheme
+import com.example.mmunozmusicapp.ui.theme.Purple
+
+@Composable
+fun Header() {
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(RoundedCornerShape(30.dp))
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        LightPurple,
+                        Purple
+                    )
+                )
+            )
+            .padding(
+                horizontal = 20.dp,
+                vertical = 16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Top
+        ) {
+            Icon(
+                imageVector = Icons.Default.Menu,
+                contentDescription = null,
+                tint = Color.White
+            )
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(bottom = 15.dp)
+                .padding(top = 20.dp)
+        ) {
+            Text(
+                text = "Good Morning!",
+                color = Color.White,
+                fontSize =  20.sp,
+            )
+            Text(
+                text = "Miguel Muñoz",
+                color = Color.White,
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HeaderView() {
+    MMunozMusicAppTheme {
+        Header()
+    }
+}
